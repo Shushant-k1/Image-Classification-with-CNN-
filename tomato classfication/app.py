a50a84cf644deb742d3c7f6bde8b942bebc31ba2
shushant kumar <104075647+everythingcanbedone@users.noreply.github.com>
2022-08-19 09:39:47 +0530

    Update app.py

--- tomato classfication/app.py
+++ tomato classfication/app.py
@@ -1,16 +1,20 @@
 import streamlit as st
 from img_classification import teachable_machine_classification
-st.title("Image Classification with Google's Teachable Machine")
+st.title("Diseases Classification on Potato's Leaf")
 st.header("Brain Tumor MRI Classification Example")
-st.text("Upload a brain MRI Image for image classification as tumor or no-tumor")
-uploaded_file = st.file_uploader("Choose a brain MRI ...", type="jpg")
+st.text("Upload a Potato leaf Image for Diseases classification as Late_Blight or Early_Blight  or Healthy")
+
+
+uploaded_file = st.file_uploader("Choose a Potato's Leaf Image ...", type="jpg")
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
         st.image(image, caption='Uploaded MRI.', use_column_width=True)
         st.write("")
         st.write("Classifying...")
-        label = teachable_machine_classification(image, 'brain_tumor_classification.h5')
+        label = teachable_machine_classification(image, 'potatoes.h5')
         if label == 0:
-            st.write("The MRI scan has a brain tumor")
+            st.write("The Potatoe's leaf is Early_Blight")
+        elif label==1:
+            st.write("The Potatoe's leaf is Late_Blight")
         else:
             st.write("The Potato leaf  is healthy")
